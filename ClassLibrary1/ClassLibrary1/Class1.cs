d19b88cdab577f1ef988bf63f6c15dce5b1817dd
Cyprian <s27165@pjwstk.edu.pl>
2024-03-13 20:35:30 +0100

    Dodano funkcje average

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -5,4 +5,19 @@ public class Class1
     private int liczba = 1;
     private int liczba2 = 2;
     private int liczba3 = 3;
+    
+    static double average ( int[] tab)
+    {
+        double srednia = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            srednia += tab[i];
+        }
+
+        srednia = srednia / tab.Length;
+        return srednia;
+
+
+
+    }
 }
